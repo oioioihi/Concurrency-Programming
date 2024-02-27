a88c3dc513d69990794a31e78d00a3cffbed0441
Boeun's <rnqhdms@gmail.com>
2024-02-27 23:51:06 +0900

    CPU Bound 예제

---
+++ src/main/java/chapter01/exam03/CPUBoundExample.java
@@ -0,0 +1,52 @@
+package chapter01.exam03;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CPUBoundExample {
+    public static void main(String[] args) {
+        int threadNum = Runtime.getRuntime().availableProcessors();
+        ExecutorService executorService = Executors.newFixedThreadPool(threadNum);
+
+        long startTime = System.currentTimeMillis();
+        List<Future<?>> futures = new ArrayList<>();
+        for (int i = 0; i < threadNum; i++) {
+            Future<?> future = executorService.submit(() -> {
+
+                //CPU 연산이 집중되고 오래 걸리는 작업
+                long result = 0;
+                for (int j = 0; j < 100000L; j++) {
+                    result += j;
+                }
+
+                // 아주 잠깐 대기함
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                }
+
+                System.out.println("스레드 : " + Thread.currentThread().getName() + ", " + result);
+                // CPU Bound 작업일 때 Context Switching
+            });
+
+            futures.add(future);
+        }
+        futures.forEach(future -> {
+            try {
+                future.get();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("CPU 개수를 초과하는 데이터를 병렬로 처리하는데 걸린 시간 : " + (endTime - startTime) + "ms");
+        executorService.shutdown();
+    }
+}
