32591e0e60d888a1883068b2f151beaa8038bc75
Boeun's <rnqhdms@gmail.com>
2024-05-06 23:08:52 +0900

    Future를 사용한 비동기 실행

---
+++ src/main/java/chapter10/exam04/FutureExample.java
@@ -0,0 +1,29 @@
+package chapter10.exam04;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureExample {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Future<Integer> future = executorService.submit(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return 2024;
+        });
+
+        try {
+            future.get(); //Blocking
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        executorService.shutdown();
+    }
+}
