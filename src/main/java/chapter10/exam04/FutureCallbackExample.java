bfdd53e75499f54087e2fbd4d4db401876440525
Boeun's <rnqhdms@gmail.com>
2024-05-08 00:22:28 +0900

    Future + Callback 을 사용한 비동기 작업 예제

--- src/main/java/chapter10/exam04/FutureCallbackExample.java
+++ src/main/java/chapter10/exam04/FutureCallbackExample.java
@@ -7,16 +7,18 @@ public class FutureCallbackExample {
         ExecutorService executorService = Executors.newFixedThreadPool(1);
 
         Callable<Integer> callableTask = () -> {
+            System.out.println(Thread.currentThread().getName() + " : start 2");
             Thread.sleep(1000);
             return 2024;
         };
         Future<Integer> future = executorService.submit(callableTask);
+        System.out.println(Thread.currentThread().getName() + " : start 1");
         System.out.println("비동기 작업 시작");
 
         registerCallback(future, result -> {
-                    System.out.println("비동기 작업 결과 : " + result);
-                }
-        );
+            System.out.println("비동기 작업 결과 : " + result);
+            System.out.println(Thread.currentThread().getName() + " : start 4");
+        });
         executorService.shutdown();
     }
 
@@ -30,6 +32,7 @@ public class FutureCallbackExample {
             } catch (ExecutionException e) {
                 throw new RuntimeException(e);
             }
+            System.out.println(Thread.currentThread().getName() + " : start 3");
             callback.onComplete(result);
         }).start();
     }
