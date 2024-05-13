21eeadd378c99c32b03675f3ef5b4c6ddf066138
Boeun's <rnqhdms@gmail.com>
2024-05-13 23:03:31 +0900

    Future API의 Get()

--- src/main/java/chapter10/exam05/FutureGetExample.java
+++ src/main/java/chapter10/exam05/FutureGetExample.java
@@ -1,7 +1,27 @@
 package chapter10.exam05;
 
+import java.util.concurrent.*;
+
 public class FutureGetExample {
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
 
+        Callable<Integer> callableTask = () -> {
+            System.out.println("비동기 작업 시작..");
+            Thread.sleep(2000);
+            System.out.println("비동기 작업 완료");
+
+            return 2024;
+        };
+        Future<Integer> future = executorService.submit(callableTask);
+        try {
+            Integer result = future.get(); //main 스레드가 result를 얻을 때 까지 blocking 됨
+            System.out.println("결과 : " + result);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        executorService.shutdown();
     }
 }
