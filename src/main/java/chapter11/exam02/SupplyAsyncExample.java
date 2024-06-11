4aa00c52b42b99d2e70bc393b1b3763d111f97cf
Boeun's <rnqhdms@gmail.com>
2024-06-11 22:30:52 +0900

    CompletableFuture - supplyAsync

--- src/main/java/chapter11/exam02/SupplyAsyncExample.java
+++ src/main/java/chapter11/exam02/SupplyAsyncExample.java
@@ -5,11 +5,16 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
+/**
+ * CompletableFuture는 비동기 작업을 실해하기 위해 내부적으로 ForkJoinPool.commonPool()의 스레드 풀을 사용하며 선택적으로 ThreadPoolExecutor를 사용할 수 있다.
+ */
 public class SupplyAsyncExample {
     public static void main(String[] args) {
 
         MyService myService = new MyService();
-        CompletableFuture<List<Integer>> cf = CompletableFuture.supplyAsync(new Supplier<List<Integer>>() {
+        CompletableFuture<List<Integer>> cf = CompletableFuture.supplyAsync(new Supplier<List<Integer>>() { // 별도의 스레드로 비동기 실행
+
+            //ForkJoinPool의 WorkQueue에 AsyncSupply객체가 저장되고, ForkJoinWorkerThread가 큐에서 작업을 꺼내어 수행함.
             @Override
             public List<Integer> get() {
                 System.out.println(Thread.currentThread().getName() + "가 비동기 작업을 시작합니다.");
