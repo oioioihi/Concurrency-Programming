7c6864a641b453ecfa7aafd5d592d55a01a4b723
Boeun's <rnqhdms@gmail.com>
2024-06-16 23:42:51 +0900

    CompletableFuture - thenAccept & thenAcceptAsync

--- src/main/java/chapter11/exam04/ThenAcceptExample.java
+++ src/main/java/chapter11/exam04/ThenAcceptExample.java
@@ -17,11 +17,12 @@ public class ThenAcceptExample {
                 throw new RuntimeException(e);
             }
             return 40;
-        }).thenAccept(r -> {
+        }).thenAccept(r -> { // 결과를 받아서 (비)동기적으로 작업 결과를 수행하고 완료 혹은 다음 작업을 수행한다.
+            // thenAccept()는 이전 작업 결과가 완료 되었다면 메인 스레드에서 동기 처리되고 그렇지 않으면 이전과 동일한 스레드에서 비동기 처리된다.
             System.out.println("Thread 2 : " + Thread.currentThread().getName());
             int result = myService.getData1();
             System.out.println("result : " + result);
-        }).thenAcceptAsync(r -> {
+        }).thenAcceptAsync(r -> { // Async 메서드이면 무조건 비동기로 처리. 이전과 동일한 스레드 혹은 새롭게 생성된 스레드가 될 수 있다.
             System.out.println("Thread 3 : " + Thread.currentThread().getName());
             int result = myService.getData2();
             System.out.println("result : " + result);
