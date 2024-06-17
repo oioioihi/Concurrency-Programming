8f71f6b189f5a0a321f9b5dd7b7dd1f739a3fdf9
Boeun's <rnqhdms@gmail.com>
2024-06-17 23:06:58 +0900

    CompletableFuture - thenCompose & thenComposeAsync

--- src/main/java/chapter11/exam05/ThenComposeExample.java
+++ src/main/java/chapter11/exam05/ThenComposeExample.java
@@ -3,12 +3,17 @@ package chapter11.exam05;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-public class ThenCombineExample {
+/**
+ * [ thenCompose ]
+ * Funtion<T,CompletionStage<U>>함수를 인수로 받고 작업 결과를 반환한다. Stream의 flatMap()과 유사하다.
+ * 실행객체 : UniCompose
+ */
+public class ThenComposeExample {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         MyService myService = new MyService();
 
         CompletableFuture<Integer> cf1 = myService.getData1(10);
-        CompletableFuture<Integer> cf2 = cf1.thenCompose(result -> {
+        CompletableFuture<Integer> cf2 = cf1.thenCompose(result -> { // 결과를 받아서 새로운 CompletableFuture를 시작하고 결과를 조작한 다음, 그 결과를 처음의 CompletableFuture에 반환한다.ㄴ
             System.out.println("cf1 result : " + result);
             System.out.println("Thread 2 : " + Thread.currentThread().getName());
             return myService.getData2(result);
