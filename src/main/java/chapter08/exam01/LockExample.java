580cd487e8d3815651373f5af06cbb8d6fd6d169
Boeun's <rnqhdms@gmail.com>
2024-10-12 23:22:04 +0900

    ReentrantLock 예제

--- src/main/java/chapter08/exam01/LockExample.java
+++ src/main/java/chapter08/exam01/LockExample.java
@@ -3,6 +3,11 @@ package chapter08.exam01;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * [ Lock ]
+ * <p>
+ * Lock 구현은 synchronized 구문과 마친가지로 상호배제와 가시성 기능을 가진 동기화 기법이며, synchronized 보다 더 확장된 락 작업을 제공한다.
+ */
 public class LockExample {
     private int count = 0;
     private Lock lock = new ReentrantLock();
@@ -33,6 +38,8 @@ public class LockExample {
 
     public void increment() {
         lock.lock();
+        // 락이 다른 스레드에 의해 보유되고 있지 않다면, 락을 즉시 획득하고 락의 보유 횟수를 1로 설정한다.
+        // 현재 스레드가 이미 이 락을 보유하고 있다면 보유 횟수가 1 증가하고 메서드는 즉시 반환된다.
         try {
             count++;
         } finally {
